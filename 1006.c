d2afc6697bf35b5808838c8688a82c238732f67a
otaviopato <otaviopato2017@outlook.com>
2020-12-12 23:19:49 -0300

    Pronto

---
+++ 1006.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+double A, B, C;
+float M;
+scanf("%lf %lf %lf", &A, &B, &C);
+M= (A*0.2+B*0.3+C*0.5);
+
+
+
+printf("MEDIA = %.1lf\n", M);
+   
+
+return 0;
+}
